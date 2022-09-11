505e88525c450475640ca33d80be9ecaa3d19afc
wzj0213 <112533766+wzj0213@users.noreply.github.com>
2022-09-12 00:12:56 +0800

    Delete searchform.php

--- searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-<input type="text" name="s" id="s" placeholder="Your Keywords" >
-            <input type="submit"  value="">
-</form>
